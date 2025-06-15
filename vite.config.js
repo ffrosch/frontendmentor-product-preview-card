1f1c6c7178a16079d9e45e2c07341b9119ffda13
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-06-15 18:28:44 +0200

    build: add base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontendmentor-product-preview-card/',
   plugins: [vue(), tailwindcss()],
 });
