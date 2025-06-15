00f568d71920f5b5179f8efeb52a232dc2fae6e6
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-06-15 18:07:59 +0200

    feat: desktop version

--- src/components/ProductPreviewCard.vue
+++ src/components/ProductPreviewCard.vue
@@ -1,14 +1,27 @@
 <script setup lang="js">
 import IconCart from '../assets/icon-cart.svg';
 import ImageProductMobile from '../assets/image-product-mobile.jpg';
+import ImageProductDesktop from '../assets/image-product-desktop.jpg';
 </script>
 
 <template>
-    <div class="bg-neutral-white rounded-xl overflow-hidden shadow-sm">
-        <img :src="ImageProductMobile" class="max-h-64 w-full object-cover" />
-        <div class="flex flex-col gap-4 p-6">
+    <div class="flex flex-col sm:flex-row max-w-2xl bg-neutral-white rounded-xl overflow-hidden shadow-sm">
+        <picture class="sm:max-w-1/2">
+            <source
+                :srcset="ImageProductDesktop"
+                media="(min-width: 640px)"
+            />
+            <img
+                :src="ImageProductMobile"
+                class="object-top max-h-64 sm:max-h-full w-full sm:h-full object-cover"
+                alt="Product Image"
+            />
+        </picture>
+
+        <div class="flex flex-col gap-4 sm:max-w-1/2 p-6 sm:p-8 sm:justify-between">
             <h2 class="text-left text-neutral-grey text-md uppercase tracking-[.25rem] font-montserrat">Perfume</h2>
-            <h3 class="text-left text-neutral-black text-4xl font-bold font-fraunces">Gabrielle Essence Eau De Parfum</h3>
+            <h3 class="text-left text-neutral-black text-4xl font-bold font-fraunces">Gabrielle Essence Eau De Parfum
+            </h3>
             <div class="text-left text-neutral-grey leading-6">
                 A floral, solar and voluptuous interpretation composed by Olivier Polge,
                 Perfumer-Creator for the House of CHANEL.
@@ -17,10 +30,18 @@ import ImageProductMobile from '../assets/image-product-mobile.jpg';
                 <span class="text-primary-500 text-3xl font-bold">$149.99</span>
                 <span class="text-neutral-grey line-through">$169.99</span>
             </div>
-            <button class="flex items-center justify-center gap-2 bg-primary-500 hover:bg-primary-700 py-3 px-4 rounded-lg">
-                <img :src="IconCart" alt="Cart Icon" height="16" width="16" />
+            <button
+                class="flex items-center justify-center gap-2 bg-primary-500 hover:bg-primary-700 py-3 px-4 rounded-lg"
+            >
+                <img
+                    :src="IconCart"
+                    alt="Cart Icon"
+                    height="16"
+                    width="16"
+                />
                 <span class="font-bold">Add to Cart</span>
             </button>
         </div>
+
     </div>
 </template>
